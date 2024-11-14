5b02c76ac15c9265eb412ba5317779f9c37f13cf
CrispyBun <74562111+CrispyBun@users.noreply.github.com>
2024-11-14 10:20:32 +0100

    Add server creation

--- Libraries/sucket.lua
+++ Libraries/sucket.lua
@@ -1,6 +1,11 @@
+local enet = require 'enet'
+
 local sucket = {}
 
 --------------------------------------------------
+--- Encoding
+--- (define these please)
+--- (or use netstring.lua)
 
 --- An encoding function for sending messages over the network.  
 --- 
@@ -19,5 +24,62 @@ sucket.encode = nil
 sucket.decode = nil
 
 --------------------------------------------------
+--- Logging
+
+---@alias sucket.Logger {log: fun(self: table, message: string, level?: string)}
+
+--- If implemented, this will be used to add a logger to all new servers and clients
+---@type fun(): sucket.Logger
+sucket.createLogger = nil
+
+--------------------------------------------------
+--- Definitions
+
+---@class Sucket.Server
+---@field host enet.host The enet host of the server
+---@field peers table<enet.peer, boolean> The peers connected to the server
+---@field logger? sucket.Logger An optional logger object with any implementation
+local Server = {}
+local ServerMT = {__index = Server}
+
+--------------------------------------------------
+--- Server
+
+---@type integer|string
+sucket.serverDefaultPort = "12832"
+
+---@type integer
+sucket.serverDefaultMaxClients = 64
+
+--- Creates and starts a new server. If for whatever reason the server isn't able to start, this function will return `nil` instead.
+---@param localOnly? boolean If true, the server will start configured to only run locally for a single client.
+---@param maxClients? integer The maximum allowed number of clients on the server. Default is the value in `sucket.serverDefaultMaxClients`.
+---@param port? integer|string The port to launch the server on. Default is `"0"` if `localOnly` is true, or otherwise the value in `sucket.serverDefaultPort`.
+---@return Sucket.Server? server
+function sucket.newServer(localOnly, maxClients, port)
+    local ip = localOnly and "localhost" or "*"
+    port = port or (localOnly and "0" or sucket.serverDefaultPort) -- port 0 is a wildcard to find any port
+    maxClients = maxClients or (localOnly and 1 or sucket.serverDefaultMaxClients)
+
+    local address = ip .. ":" .. port
+
+    local success, host = pcall(enet.host_create, address, maxClients)
+    if not success then return nil end
+    if not host then return nil end
+
+    ---@type Sucket.Server
+    local server = {
+        host = host,
+        peers = {},
+    }
+
+    if sucket.createLogger then
+        local logger = sucket.createLogger()
+        logger:log(string.format("Server created at %s", tostring(server.host:get_socket_address())), "info")
+        server.logger = logger
+    end
+
+    return server
+end
 
 return sucket
