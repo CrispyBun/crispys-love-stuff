380aa4a40fa06a27eaade0d687a5aaf70fdb3117
CrispyBun <74562111+CrispyBun@users.noreply.github.com>
2024-11-25 09:31:23 +0100

    Add simple way of adding translations

--- Libraries/latl.lua
+++ Libraries/latl.lua
@@ -3,8 +3,10 @@
 local latl = {}
 local latlMT = {}
 
+--- The currently selected language used as default when getting translations
 latl.currentLanguage = "en_US"
 
+--- All of the defined languages
 ---@type Latl.LanguageList
 latl.langs = {}
 
@@ -12,18 +14,18 @@ latl.langs = {}
 --- Definitions
 
 ---@class Latl.LanguageList
----@field [string] Latl.Language Each language code (e.g. "en_US") mapped to its language definition
+---@field [string] Latl.Language Each language code (e.g. `"en_US"`) mapped to its language definition
 
 --- A set of translations
 ---@class Latl.Language
----@field [string] string Each text identifier (e.g. "game.items.sword") mapped to its translation (e.g. "Sword")
+---@field [string] string Each text identifier (e.g. `"game.items.sword"`) mapped to its translation (e.g. `"Sword"`)
 
 --------------------------------------------------
 --- Getting translations
 
 --- Gets the translation of the given `prompt`. Will use `latl.currentLanguage` if `langcode` isn't provided.
 --- You can also call this function using `latl()`.
----@param prompt string The identifier of the translation (e.g. "game.items.sword")
+---@param prompt string The identifier of the translation (e.g. `"game.items.sword"`)
 ---@param langcode? string
 ---@return string
 function latl.get(prompt, langcode)
@@ -51,6 +53,20 @@ end
 ---@param langcode string
 function latl.setCurrentLanguage(langcode)
     latl.currentLanguage = langcode
+end
+
+--------------------------------------------------
+--- Defining translations
+
+--- Adds a single translation to the specified language.
+---@param langcode string The language code of the language (e.g. `"en_US"`)
+---@param textID string The identifier of the translation (e.g. `"game.items.sword"`)
+---@param translation string The translation itself (e.g. `"Sword"`)
+function latl.addTranslation(langcode, textID, translation)
+    local language = latl.langs[langcode] or {}
+    latl.langs[langcode] = language
+
+    language[textID] = translation
 end
 
 --------------------------------------------------
